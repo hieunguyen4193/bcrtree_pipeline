147b14701cdfe8b977a122daffdfbce54e665a5a
trnguyenuka <trnguyenuka@github.com>
2025-11-12 16:38:19 +0100

    finish sh script for running gctree

---
+++ GCTree_pipeline_bash.sh
@@ -0,0 +1,46 @@
+# install GCTree.
+# see instruction to install GCTree https://matsen.group/gctree/install.html
+# probably the best way is to use conda or pip install gctree
+# install PHYLIP: conda install -c bioconda phylip
+samplesheet="SampleSheet_FASTA.csv";
+deduplicate_src="./deduplicated.py"
+outputdir="./output/GCTree"
+
+mkdir -p ${outputdir}
+# if you don't use nextflow as a pipeline orchestration engine, run this bash script. 
+while IFS=',' read -r sample_id fasta; do \
+    mkdir -p ${outputdir}/${sample_id};
+    echo -e "working on FASTA file " $sample_id " at " $fasta;
+    fasta_name=$(echo ${fasta} | xargs -n 1 basename);
+
+    cat ${fasta} | sed "s/>.*|Abundance:\\([0-9]\\+\\)/>\\1/" > ${outputdir}/${sample_id}/${fasta_name}.modified.fa
+    python ${deduplicate_src} \
+        --input ${outputdir}/${sample_id}/${fasta_name}.modified.fa \
+        --root GL \
+        --frame 0 \
+        --id_abundances \
+        --output_name ${sample_id} \
+        --output ${outputdir}/${sample_id}
+
+    mkconfig --quick ${outputdir}/${sample_id}/${sample_id}.phylip dnapars > ${outputdir}/${sample_id}/${sample_id}_dnapars.cfg
+    dnapars < ${outputdir}/${sample_id}/${sample_id}_dnapars.cfg > ${outputdir}/${sample_id}/${sample_id}_dnapars.log
+
+    # since outfile and outtree are generated at the current working dir, we need to manually move it to the output dir. 
+    mv outfile ${outputdir}/${sample_id};
+    mv outtree ${outputdir}/${sample_id};
+    export QT_QPA_PLATFORM=offscreen
+    export XDG_RUNTIME_DIR=/tmp/runtime-runner
+    export MPLBACKEND=agg
+
+    gctree infer --verbose --root GL --frame 1 --idlabel ${outputdir}/${sample_id}/outfile ${outputdir}/${sample_id}/${sample_id}.abundance.csv
+done < <(tail -n +2 ${samplesheet})
+
+
+# test read in samplesheet file with sample id and path to file fasta. 
+# samplesheet="SampleSheet_FASTA.csv";
+# # if you don't use nextflow as a pipeline orchestration engine, run this bash script. 
+# while IFS=',' read -r sampleid fasta; do \
+#     echo $sampleid 
+#     echo $fasta
+# done < <(tail -n +2 ${samplesheet})
+#####
---
+++ install_gctree.sh
@@ -0,0 +1,4 @@
+# conda create -n gctree python=3.9 -y
+# conda activate gctree
+# pip install gctree
+# conda install -c bioconda phylip -y
